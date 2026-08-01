ff7e3c080a40b6ebaaacfba84df294b40c1d6d47
Akashkumargiri1612 <akashkumer1612@gmail.com>
2026-08-01 03:43:37 +0000

    Learn JavaScript Basics

---
+++ 01_Basic/Data.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as newer version
+
+// alert (3+3) // we are using the node.js, not browser 
+
+// numder => 2 to pwr 53
+// bigint
+// string =>" "
+// boolean => true/false
+// null => standalone value,empty value
+// undefined => no value
+//symbol => unique 
+
+
+// object
+
+console.log(typeof "Akash")
+console.log(typeof null)
+console.log(typeof undefined);
+
+
+
---
+++ 01_Basic/variable.js
@@ -0,0 +1,18 @@
+const accountId = 14456
+let accountemail ="akashkumer1612@gmail.com"
+var accountPassword ="1612"
+accountCity = "Hyderbad"
+let account_state;
+
+// accountId = 2 // not allowed
+accountemail = "abcdfewg"
+accountPassword ="3355"
+accountCity = "pune"
+/*
+Pefer not to used the var because of issue in the block scope and functional sc
+*/ 
+
+
+console.log(accountId);
+console.table([accountId,accountPassword,accountemail,accountCity])
+console.log(account_state);
