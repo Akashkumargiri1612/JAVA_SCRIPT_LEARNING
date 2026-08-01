ede81985418bd4c072fa7c30f6775e952a565384
Akashkumargiri1612 <akashkumer1612@gmail.com>
2026-08-01 05:11:14 +0000

    Learning JavaScript

---
+++ 01_Basic/conversation.js
@@ -0,0 +1,57 @@
+let score = 33
+
+console.log(typeof score);
+console.log(typeof(score));
+
+numberintValue = "33abc"
+console.log(numberintValue);
+console.log(typeof numberintValue);
+
+convertStringNumber = Number(numberintValue)
+console.log(convertStringNumber);
+console.log(typeof convertStringNumber);
+
+
+// "33"=> 33
+// "33abd" => NaN
+
+
+booleanVariable = true
+console.log(booleanVariable);
+console.log(typeof booleanVariable);
+
+numberbooleanValue = Number(booleanVariable)
+console.log(numberbooleanValue);
+console.log(typeof numberbooleanValue);
+
+
+// true => 1; false => 0
+//  boolean study the 1 => true; 0 => false
+
+
+ValueString = ""
+console.log(typeof ValueString);
+
+booleantoString = Boolean(ValueString)
+console.log(booleantoString);
+
+
+// "" => false
+//  "Akash" => true
+
+
+nullValue = null
+console.log(nullValue);
+
+boolnullValue = Boolean(nullValue)
+console.log(boolnullValue);
+
+numbernullValue = Number(nullValue)
+console.log(numbernullValue);
+console.log(typeof numbernullValue);
+
+
+// null => null
+// null => boolean => false
+// null => number => 0
+
