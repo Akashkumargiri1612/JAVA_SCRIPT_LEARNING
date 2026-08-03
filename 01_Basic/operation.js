efcb2c04d95bc6b89d2ecf159441d93a235aa84b
Akash kumar giri <akashkumer1612@gmail.com>
2026-08-03 09:36:22 +0530

    Add files via upload

---
+++ 01_Basic/operation.js
@@ -0,0 +1,53 @@
+let value = 3
+let negvalue = -value
+
+// console.log(negvalue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2**2);
+console.log(2%2);
+
+// Basic operations
+
+let str1 = "hello"
+let str2 = " akash"
+let str3 = str1 + str2
+console.log(str3);
+
+// String Concatenation
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+console.log("1"+"2"+"2")
+
+// Concatenation differences
+
+console.log(+true);
+console.log(+"");
+
+let gameCounter = 100 
+let x = ++gameCounter;
+console.log(gameCounter);
+console.log(x);
+
+// In the prefix the value is increment first and then printed
+
+let incValue = 101
+let y = incValue++;
+console.log(incValue);
+console.log(y);
+
+// In the postfix the value is printed frist then incremented
+ 
+
+
+
+
+
+
+
