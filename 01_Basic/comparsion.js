2326ddab5d08da26d321c4886345a1c4ba777e47
Akash kumar giri <akashkumer1612@gmail.com>
2026-08-03 04:38:38 +0000

    Learning java_script

---
+++ 01_Basic/comparsion.js
@@ -0,0 +1,24 @@
+console.log(2 > 2);
+console.log(2 < 3);
+console.log(2 == 2);
+console.log(2 != 3);
+console.log(2 >= 3);
+console.log(3 <= 3);
+
+// Basic comparsion operation 
+
+console.log(null == 0);
+console.log(null < 0);
+console.log(null > 0);
+console.log(null != 0);
+
+// NULL comparsion with zero
+
+console.log(null == undefined);
+
+// null and undefined comparsion
+
+console.log("2" == 2);
+console.log("2" === 2);
+
+// comparsion equality and Strict Equality
